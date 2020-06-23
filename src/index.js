bac8aee34fb28a55e9c3c06ccd7529ba08f085a2
RimshaAli <rimsha.bsse3429@iiu.edu.pk>
2020-06-24 01:56:51 +0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 import './index.css';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <center> 
-    <div>Hello World.</div>
-    </center>,
-
- 
+  <App/>,
   document.getElementById('root')
 );
 
